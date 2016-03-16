6236ec46110d741e3506da695a0e3e90b067cc31
Andhitia Rama <andhitia.r@gmail.com>
2016-03-16 15:02:56 +0700

    [ADD][8.0] brd_warehouse_reporting

---
+++ brd_warehouse_reporting/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Andhitia Rama <andhitia.r@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Brodo Warehouse Reporting',
+    'version': '8.0.1.0.0',
+    'category': 'Warehouse',
+    'author': 'Andhitia Rama,OpenSynergy Indonesia',
+    'website': 'https://opensynergy-indonesia.com',
+    'data': [
+        'security/data_Application.xml',
+        'menu/menu_Report.xml',
+    ],
+    'depends': [
+        'brd_reporting',
+        ],
+    "installable": True,
+    "auto_install": False,
+}
